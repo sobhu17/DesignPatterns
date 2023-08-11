1915e15c291aa1e9eff295ded217250369f72193
saurabh kaushik <habsaurabh1702@gmail.com>
2023-08-11 14:46:26 +0530

    Singleton Design Pattern

---
+++ src/DoubleCheckLocking/Client.java
@@ -0,0 +1,36 @@
+package DoubleCheckLocking;
+
+public class Client {
+    public static void main(String[] args) {
+        DataBase db1 = DataBase.createInstance();
+        System.out.println(db1);
+
+        DataBase db2 = DataBase.createInstance();
+        System.out.println(db2);
+
+
+            /*
+        public synchronized static DataBase createInstance(){
+        if(instance == null){
+            instance = new DataBase();
+        }
+
+        return instance;
+    }
+
+        this is the correct way of handling multithreaded environment but here we are doing way too much
+        as we are taking lock everytime a new thread come to access this method
+        But we only need lock for the first few threads, once our object get created we don't need to take lock
+            again and again.
+    */
+
+
+     /*
+        Here we are using double check locking like the GF and BF example
+        Check twice before going out with the girl whether she is single or not
+        same with object creation check if the object is created or not two times to confirm
+     */
+
+
+    }
+}
---
+++ src/DoubleCheckLocking/DataBase.java
@@ -0,0 +1,20 @@
+package DoubleCheckLocking;
+
+public class DataBase {
+    private static DataBase instance = null;
+
+    private DataBase(){
+
+    }
+
+    public static DataBase createInstance(){
+        if(instance == null){
+            synchronized (DataBase.class){
+                if(instance == null){
+                    instance = new DataBase();
+                }
+            }
+        }
+        return instance;
+    }
+}
